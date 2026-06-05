60017bc63c8dff4132b6c38b446c6ffba8720b67
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-06-05 02:28:28 +0100

    Create initial FastAPI backend scaffold

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from app.routers import health
 
+# instantiate app
 app = FastAPI()
+
+# include routers 
+app.include_router(health.router)
+
+@app.get("/")
+def root():
+  return {"message":"API running"}
+
+
+
 
 
 
