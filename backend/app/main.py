709971c0347d5fb8105dca9be3439ff2a7e70a05
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-06-08 00:36:34 +0100

    Link fronted to backend via CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import health
+from app.routers import auth
+from app.database import Base, engine
+from app.models import User
 
 # instantiate app
 app = FastAPI()
 
+# allow frontend to call backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.on_event("startup")
+def startup():
+  Base.metadata.create_all(bind=engine)
+
 # include routers 
 app.include_router(health.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
