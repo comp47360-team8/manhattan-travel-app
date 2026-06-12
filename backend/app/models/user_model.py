20969001777fa74212c80cef771a4b9fe6197a8c
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-06-12 09:18:16 +0100

    Create new models folder for housing SQLAlchemy ORM models and place user model in new user_model.py file.

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,37 @@
+import uuid
+from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import UUID
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), 
+        primary_key=True, 
+        default=uuid.uuid4
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(255), 
+        unique=True, 
+        nullable=False, 
+        index=True
+    )
+
+    password_hash: Mapped[str] = mapped_column(
+        String(255), 
+        nullable=False
+        )
+
+    display_name: Mapped[str] = mapped_column(
+        String(50), 
+        nullable=False
+        )
+
+    created_at: Mapped[DateTime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        nullable=False
+    )
