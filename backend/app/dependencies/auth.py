a69203803d500edce97084972dfae0a76ff33aec
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-06-20 00:22:24 +0100

    Add access token extraction from request headers

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,11 +1,26 @@
-from fastapi import Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends, HTTPException, status, Header, Cookie
 from app.core.security.tokens import decode_token
 from app.core.exceptions import AuthenticationError
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+def get_access_token(authorization: str | None = Header(None), access_token: str | None = Cookie(None)):
+    token = None
 
-def authorise_access(token: str = Depends(oauth2_scheme)):
+    # MOBILE 
+    if authorization and authorization.startswith("Bearer "):
+        token = authorization.split(" ")[1]
+    
+    # WEB
+    elif access_token:
+        token = access_token
+
+    if not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access token missing."
+        )
+    return token
+
+def authorise_access(token: str = Depends(get_access_token)):
     try:
         payload = decode_token(token)
 
@@ -21,3 +36,4 @@ def authorise_access(token: str = Depends(oauth2_scheme)):
             detail="Not authorised for this resource. Please log in first.",
         )
     
+    return payload.get("sub")
