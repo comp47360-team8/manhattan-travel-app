4d2bd6f1bd64ef715632aef097c6d7574c33d06f
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-06-21 16:23:17 +0100

    Add save and unsave POI endpoints

--- backend/app/routers/pois.py
+++ backend/app/routers/pois.py
@@ -1,17 +1,21 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.services.poi_service import get_all_pois, get_poi_by_slug
+from app.services.poi_service import get_all_pois, get_poi_by_slug, unsave_poi_for_user
+from app.services.poi_service import save_poi_for_user
+from app.core.exceptions import POINotFoundError
+from app.dependencies.auth import authorise_access
+from app.schemas.poi import POIDetailedResponse, POISaveResponse, POIUnsaveResponse
 
 router = APIRouter(prefix="/pois", tags=["pois"])
 
-@router.get("")
-def get_pois(db: Session = Depends(get_db)):
+@router.get("", response_model=list[POIDetailedResponse])
+def get_pois(db:Session = Depends(get_db)):
     return get_all_pois(db)
 
 
-@router.get("/{slug}")
-def get_poi(slug:str, db: Session = Depends(get_db)):
+@router.get("/{slug}", response_model=POIDetailedResponse)
+def get_poi(slug:str, db:Session = Depends(get_db)):
     poi = get_poi_by_slug(slug, db)
     if poi is None:
         raise HTTPException(
@@ -19,7 +23,31 @@ def get_poi(slug:str, db: Session = Depends(get_db)):
             detail="Destination not found."
         )
     return poi
+
+@router.post("/{slug}/save", response_model=POISaveResponse)
+def save_poi(slug:str , db:Session = Depends(get_db), user = Depends(authorise_access)):
+    try:
+        save_poi_for_user(slug, db, user)
+        return {"message": "Attraction saved."}
+
+    except POINotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Attraction not found."
+        )
     
+@router.delete("/{slug}/save", response_model=POIUnsaveResponse)
+def unsave_poi(slug:str, db:Session = Depends(get_db), user = Depends(authorise_access)):
+    try:
+        unsave_poi_for_user(slug, db, user)
+        return {"message": "Attraction unsaved."}
+    
+    except POINotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Attraction not found."
+        )
+
     
 
     
