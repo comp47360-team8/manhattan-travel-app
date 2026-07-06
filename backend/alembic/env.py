2185f11baf53c6163010ec849e1e373b961465f0
Hansel Oduah <hansel@MACBOOKAIR-3406.station>
2026-07-06 10:02:24 +0100

    Add versions for saved_itinerary table

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -9,6 +9,8 @@ from app.core.config import settings
 from app.database import Base
 from app.models.user_model import User, UserSession
 from app.models.poi_model import POI, SavedPOI, POIBusynessForecast
+from app.models.itinerary_model import SavedItineraries
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
