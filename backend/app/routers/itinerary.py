71cb146bc3270beb6e3d808b00bc9828ca845f3e
Hansel Oduah <hansel.oduah@ucdconnect.ie>
2026-07-09 22:36:03 +0100

    Implement add/delete poi from itinerary feature

--- backend/app/routers/itinerary.py
+++ backend/app/routers/itinerary.py
@@ -3,10 +3,13 @@ from sqlalchemy.orm import Session
 from app.schemas.itinerary import ItineraryRequest
 from app.services.itinerary.itinerary_service import create_itinerary
 from app.database import get_db
-from app.core.exceptions import MaximumPOIsExceeded, POINotOpenDuringTrip
+from app.core.exceptions import MaximumPOIsExceeded, POINotOpenDuringTrip, StopNotFound, RepeatingPOI, ItineraryNotFound, POINotFoundError
 from app.dependencies.auth import authorise_access
-from app.schemas.itinerary import ItineraryResponse, ItinerarySaveResponse, ItineraryUnsaveResponse
-from app.repositories.itinerary_repository import save_itinerary_for_user, unsave_itinerary_for_user, serialize_itinerary
+from app.schemas.itinerary import ItineraryResponse, ItinerarySavedResponse, ItineraryUnsaveResponse, AddStopRequest
+from app.repositories.itinerary_repository import (
+    save_itinerary_for_user, unsave_itinerary_for_user, 
+    serialize_itinerary, create_new_request,
+    update_saved_itinerary)
 
 router = APIRouter(prefix="/api/itinerary", tags=["itinerary"])
 
@@ -26,14 +29,23 @@ def generate_itinerary(request: ItineraryRequest, db: Session = Depends(get_db))
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e)
         )
+    except RepeatingPOI:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"You cannot enter the same POI more than once. Please try again."
+        )
     
-@router.post("")
+@router.post("", response_model=ItinerarySavedResponse)
 def save_itinerary(request: ItineraryResponse, db: Session = Depends(get_db), user = Depends(authorise_access)):
     try:
         itinerary = save_itinerary_for_user(request, db, user)
         return serialize_itinerary(itinerary)
+    
     except Exception as e:
-        raise e
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Something went wrong while processing your itinerary: {str(e)}"
+        )
     
 @router.delete("/{itinerary_id}", response_model=ItineraryUnsaveResponse)
 def unsave_itinerary(itinerary_id, db: Session = Depends(get_db), user = Depends(authorise_access)):
@@ -42,14 +54,52 @@ def unsave_itinerary(itinerary_id, db: Session = Depends(get_db), user = Depends
         return ItineraryUnsaveResponse(
             message="Itinerary unsaved."
         )
+    
+    except ItineraryNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Itinerary not found."
+        )
+    
     except Exception as e:
-        raise e
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Something went wrong while processing your itinerary: {str(e)}"
+        )
+
+@router.post("/{itinerary_id}/stops", response_model=ItinerarySavedResponse)
+def add_stop(itinerary_id, request: AddStopRequest, db: Session = Depends(get_db), user = Depends(authorise_access)):
+    try:
+        new_request = create_new_request(itinerary_id, request.slug, None, db, user)
+        new_itinerary = generate_itinerary(new_request, db)
+        return update_saved_itinerary(new_itinerary, itinerary_id, db, user)
     
-
-
-
+    except POINotFoundError:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="POI not found."
+        )
+    
+    except ItineraryNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Itinerary not found."
+        )
+    
+@router.delete("/{itinerary_id}/stops/{stop_id}", response_model=ItinerarySavedResponse)
+def delete_stop(itinerary_id, stop_id, db: Session = Depends(get_db), user = Depends(authorise_access)):
+    try:
+        new_request = create_new_request(itinerary_id, None, stop_id, db, user)
+        new_itinerary = generate_itinerary(new_request, db)
+        return update_saved_itinerary(new_itinerary, itinerary_id, db, user)
         
-
-    
-    
-
+    except StopNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Stop not found."
+        )
+    except ItineraryNotFound:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Itinerary not found."
+        )
--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -5,7 +5,7 @@ from app.services.poi_service import get_saved_pois
 from app.dependencies.auth import authorise_access
 from app.schemas.poi import POIDetailedResponse
 from app.repositories.itinerary_repository import get_saved_itineraries, get_saved_itinerary
-from app.schemas.itinerary import ItineraryResponse, ItinerarySavedResponse
+from app.schemas.itinerary import ItinerarySummaryResponse, ItinerarySavedResponse
 from app.core.exceptions import ItineraryNotFound
 
 router = APIRouter(prefix="/api/users/me", tags=["users"])
@@ -14,14 +14,15 @@ router = APIRouter(prefix="/api/users/me", tags=["users"])
 def display_saved_pois(db: Session = Depends(get_db), user=Depends(authorise_access)):
     return get_saved_pois(db, user)
 
-@router.get("/saved-itineraries", response_model=list[ItinerarySavedResponse])
+@router.get("/saved-itineraries", response_model=list[ItinerarySummaryResponse])
 def display_saved_itineraries(db: Session = Depends(get_db), user=Depends(authorise_access)):
     return get_saved_itineraries(db, user)
 
-@router.get("/saved-itineraries/{itinerary_id}", response_model=ItineraryResponse)
+@router.get("/saved-itineraries/{itinerary_id}", response_model=ItinerarySavedResponse)
 def display_saved_itinerary(itinerary_id, db: Session = Depends(get_db), user=Depends(authorise_access)):
     try:
         return get_saved_itinerary(itinerary_id, db, user)
+    
     except ItineraryNotFound:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
