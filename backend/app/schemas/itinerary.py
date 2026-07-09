f55d77f90b74c81af2498775f6047b007ee9e1e8
Hansel Oduah <hansel.oduah@ucdconnect.ie>
2026-07-09 22:37:21 +0100

    Create pydantic response and request models for add/delete poi feature

--- backend/app/schemas/itinerary.py
+++ backend/app/schemas/itinerary.py
@@ -5,7 +5,7 @@ class ItineraryRequest(BaseModel):
     trip_name: str
     trip_dates: list[date]
     pois: list
-    accessibilty: list[str | None]
+    accessibility: list[str | None]
 
 class BusynessResponse(BaseModel):
     hour_of_day: int
@@ -26,7 +26,7 @@ class StopResponse(BaseModel):
     hero_image_url: str
     borough: str
     neighborhood: str
-    suggested_duration: str
+    suggested_duration: int
     accessibility: list
     flags: list[str]
     busyness_for_day: list[BusynessResponse]
@@ -37,20 +37,51 @@ class ItineraryResponse(BaseModel):
     start_date: date
     end_date: date
     warning: str | None
+    accessibility: list[str]
+    warning: str | None
     stops: list[StopResponse]
-
-class ItinerarySaveResponse(BaseModel):
-    message: str
 
 class ItineraryUnsaveResponse(BaseModel):
     message: str
 
+class ItinerarySummaryResponse(BaseModel):
+    itinerary_id: str
+    trip_name: str
+    start_date: date
+    end_date: date
+    number_of_places: int
+    hero_image_url: str | None
+
+class ItinerarySavedStopsResponse(BaseModel):
+    stop_id: str
+    poi_id: int
+    poi_name: str
+    slug: str
+    day_number: int
+    visit_date: date
+    slot: str
+    slot_start: time
+    slot_end: time
+    position: int
+    poi_type: str
+    crowd_level: str
+    hero_image_url: str
+    borough: str
+    neighborhood: str
+    suggested_duration: int
+    accessibility: list[str] | None
+    flags: list[str]
+    busyness_for_day: list[BusynessResponse]
+
 class ItinerarySavedResponse(BaseModel):
     itinerary_id: str
     trip_name: str
-    trip_dates: str
-    number_of_places: int
-    hero_image_url: str
-    
+    start_date: date
+    end_date: date
+    warning: str | None
+    stops: list[ItinerarySavedStopsResponse]
+
+class AddStopRequest(BaseModel):
+    slug: str
 
 
