21350d90bb2dabe0173bab241ad4dbc0aeab97eb
Hansel Oduah <hansel.oduah@ucdconnect.ie>
2026-07-16 11:09:52 +0100

    Return user's display name and accessibility status upon signup and login

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,11 +5,11 @@ from pydantic_core import PydanticCustomError
 class UserBase(BaseModel):
     email: EmailStr
     display_name: str
+    accessibility: bool
 
 class UserCreate(UserBase):
     password: str
     confirm_password: str
-    accessibility: bool
 
     @field_validator("password")
     @classmethod
