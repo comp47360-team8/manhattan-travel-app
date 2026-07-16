649fb941e07d95a24d95c9c81cb5fb31b29c677e
Hansel Oduah <hansel.oduah@ucdconnect.ie>
2026-07-16 13:36:30 +0100

    Allow user to swap poi preference and excluded types

--- backend/app/repositories/trip_repository.py
+++ backend/app/repositories/trip_repository.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 from app.models.ai_model import Trip, TripExcludedPOI
 from app.schemas.ai import TripParameters
 from app.services.poi_service import get_pois_by_slug
-from app.repositories.ai_repository import get_conversation_by_id
 
 def get_trip(conv_id, db: Session):
     statement = select(Trip).where(Trip.conversation_id == conv_id)
@@ -25,19 +24,20 @@ def update_trip(conv_id: str, extracted: TripParameters, exclude_pois: list[str]
     if extracted.pace:
         trip.pace = extracted.pace
 
-    print(f"excluded types before are: {trip.excluded_types}")
     if extracted.excluded_types:
         for poi_type in extracted.excluded_types:
             if poi_type not in trip.excluded_types:
                 trip.excluded_types.append(poi_type)
-    print(f"excluded types after are: {trip.excluded_types}")
+            if poi_type in trip.preferences:
+                trip.preferences = [item for item in trip.preferences if item != poi_type]
 
-    print(f"preferences before are: {trip.preferences}")
     if extracted.preferences:
         for preference in extracted.preferences:
             if preference not in trip.preferences:
                 trip.preferences.append(preference)
-    print(f"preferences after are: {trip.preferences}")
+            if preference in trip.excluded_types:
+                trip.excluded_types = [item for item in trip.excluded_types if item != preference]
+   
 
     if exclude_pois:
         statement2 = select(TripExcludedPOI.poi_id).join(Trip).where(
